d36a7e41d093f279e83a1d002adf272bc5b0a863
Barkah Aji <barkah993@gmail.com>
2025-05-20 14:14:43 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+let posts = require('./posts.json');
+
+// GET all posts
+app.get('/posts', (req, res) => {
+  res.json(posts);
+});
+
+// POST a new blog post
+app.post('/posts', (req, res) => {
+  const newPost = {
+    id: Date.now(),
+    title: req.body.title,
+    content: req.body.content
+  };
+  posts.push(newPost);
+  fs.writeFileSync('./posts.json', JSON.stringify(posts, null, 2));
+  res.status(201).json(newPost);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
